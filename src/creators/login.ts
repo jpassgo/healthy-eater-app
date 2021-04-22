4d861841dfa7f31aaab205fed303b29fc7c36c38
jpassgo <jeffpascoe9@gmail.com>
2021-04-22 17:02:01 -0500

    Adding loginAttempt action.

--- src/creators/login.ts
+++ src/creators/login.ts
@@ -1,6 +1,14 @@
 import actions from '../actions/actions';
 import { AnyAction } from 'redux';
 
+export const loginAttempt = (username: string, password: string): AnyAction => {
+    return {
+        type: actions.LOGIN_ATTEMPT,
+        username: username,
+        password: password
+    }
+}
+
 export const loginSuccess = (): AnyAction => {
     return {
         type: actions.LOGIN_SUCCESS,
