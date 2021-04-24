57607832c4ddcf0477408e801456f27813e6b7d5
jpassgo <jeffpascoe9@gmail.com>
2021-04-23 19:35:14 -0500

    Updating App uing MuiThemeProvider to inject the redux store.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { Component } from "react";
+import LoginPage from "./components/LoginPage";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import { MuiPickersUtilsProvider } from "@material-ui/pickers";
+import MomentUtils from "@date-io/moment";
+import createMuiTheme, { Theme } from "@material-ui/core/styles/createMuiTheme";
+import { State } from "./config/store";
+import { connect } from "react-redux";
 
-function App() {
-  return (
-    <div>
-      <header>
-      
-      </header>
-    </div>
-  );
+export function getLightTheme(): Theme {
+  return createMuiTheme();
 }
 
-export default App;
+const App = (props: AppProps): JSX.Element => {
+  return (
+    <MuiThemeProvider theme={getLightTheme()}>
+      <LoginPage />
+    </MuiThemeProvider>
+  );
+};
+
+export interface AppProps {
+  isAuthenticated: boolean;
+}
+
+const mapStateToProps = (state: State): AppProps => {
+  return ({
+    isAuthenticated: state.applicationState.isAuthenticated,
+  } as unknown) as AppProps;
+};
+
+const mapDispatchToProps = (): AppProps => (({} as unknown) as AppProps);
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
