bd7574f1621bd1bd85ba359e97fb59eda1ae8411
jpassgo <jeffpascoe9@gmail.com>
2021-04-26 20:59:59 -0500

    Adding a navigationd drawer that opens from the left.

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,57 +1,101 @@
-import { State } from "../config/store";
-import React from "react";
-import classes from "*.module.css";
+import { State } from '../config/store';
+import React from 'react';
 import {
-  AppBar,
-  Toolbar,
-  IconButton,
-  Typography,
   Button,
-} from "@material-ui/core";
-import { connect } from "http2";
-import MenuIcon from "@material-ui/icons/Menu";
-import { Dispatch } from "redux";
+  makeStyles,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Divider,
+  SwipeableDrawer,
+} from '@material-ui/core';
+import { connect } from 'react-redux';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
 
-const NavigationBar = (navigationBarProps: NavigationBarProps) => {
+import { Dispatch } from 'redux';
+
+const useStyles = makeStyles({
+  list: {
+    width: 250,
+  },
+  fullList: {
+    width: 'auto',
+  },
+});
+
+const NavigationDrawer = (navigationDrawerProps: NavigationDrawerProps) => {
+  const classes = useStyles();
+  const [state, setState] = React.useState({
+    isMenuOpen: false,
+  });
+
+  const isMenuOpen = false;
+
+  const toggleDrawer = (open: boolean) => (
+    event: React.KeyboardEvent | React.MouseEvent
+  ) => {
+    if (
+      event &&
+      event.type === 'keydown' &&
+      ((event as React.KeyboardEvent).key === 'Tab' ||
+        (event as React.KeyboardEvent).key === 'Shift')
+    ) {
+      return;
+    }
+
+    setState({ ...state, isMenuOpen: open });
+  };
+
+  const list = () => (
+    <div
+      role='presentation'
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+    >
+      <List>
+        {['Login', 'Account', 'Report Meal', 'Calorie Search'].map(
+          (text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          )
+        )}
+      </List>
+      <Divider />
+    </div>
+  );
+
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
+    <div>
+      <React.Fragment key={'Menu'}>
+        <Button onClick={toggleDrawer(true)}>Menu</Button>
+        <SwipeableDrawer
+          open={isMenuOpen}
+          onClose={toggleDrawer(false)}
+          onOpen={toggleDrawer(true)}
+        ></SwipeableDrawer>
+      </React.Fragment>
     </div>
   );
 };
 
-interface NavigationBarProps {
+interface NavigationDrawerProps {
   isAuthenticated: boolean;
 }
 
-const mapStateToProps = (state: State): NavigationBarProps => {
-    return(
-        {
-            isAuthenticated: state.applicationState.isAuthenticated,
-        }
-    );
+const mapStateToProps = (state: State): NavigationDrawerProps => {
+  return ({
+    isAuthenticated: state.applicationState.isAuthenticated,
+  } as unknown) as NavigationDrawerProps;
 };
 
 const mapDispatchToProps = (dispatch: Dispatch) => {
-    return(
-       {} as unknown
-    ) as NavigationBarProps;
-}
+  return ({} as unknown) as NavigationDrawerProps;
+};
 
-
-export default connect(mapStateToProps, mapDispatchToProps)(NavigationBar);
+export default connect(mapStateToProps, mapDispatchToProps)(NavigationDrawer);
