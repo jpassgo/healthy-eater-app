98e60e2eaba1a5bb996b22e16875217c70c7be3f
jpassgo <jeffpascoe9@gmail.com>
2021-05-02 21:24:10 -0500

    Wrapping the login input fields and login button in a card.

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -2,14 +2,31 @@ import { ChangeEvent, useState } from "react";
 import { Dispatch } from "redux";
 import Grid from "@material-ui/core/Grid";
 import React from "react";
+import Card from '@material-ui/core/Card';
 import { TextField, Button } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import CardContent from '@material-ui/core/CardContent';
 import { State } from "../config/store";
 import { connect } from "react-redux";
 import { loginAttempt } from "../creators/login";
+import Typography from "@material-ui/core/Typography";
 
 const styles = makeStyles(() => ({
   gridContainer: {},
+  root: {
+    maxWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  }
 }));
 
 interface LoginPageState {
@@ -18,6 +35,7 @@ interface LoginPageState {
 }
 
 const LoginPage = (props: LoginPageProps): JSX.Element => {
+  const classes = styles();
   const [state, setState] = useState<LoginPageState>({
     username: "",
     password: "",
@@ -38,39 +56,51 @@ const LoginPage = (props: LoginPageProps): JSX.Element => {
     props.handleLoginClick(state.username, state.password);
   };
 
-  return (
-    <Grid
-      container
-      direction={"column"}
-      alignItems={"center"}
-      justify={"center"}
-    >
-      <form>
-        <Grid item>
-          <TextField
-            id="outlined-username-input"
-            label="Username"
-            autoComplete="current-username"
-            variant="outlined"
-          />
-        </Grid>
+  
 
-        <Grid item>
-          <TextField
-            id="outlined-password-input"
-            label="Password"
-            type="password"
-            autoComplete="current-password"
-            variant="outlined"
-          />
+  return (
+    
+        <Grid
+          container
+          direction={"column"}
+          alignItems={"center"}
+          justify={"center"}
+        >
+
+          <Card className={classes.root} >
+            <CardContent>
+              <Typography className={classes.title} color="textSecondary" gutterBottom>
+                Login
+              </Typography>
+              <form>
+                <Grid item>
+                  <TextField
+                    id="outlined-username-input"
+                    label="Username"
+                    autoComplete="current-username"
+                    variant="outlined"
+                  />
+                </Grid>
+
+                <Grid item>
+                  <TextField
+                    id="outlined-password-input"
+                    label="Password"
+                    type="password"
+                    autoComplete="current-password"
+                    variant="outlined"
+                  />
+                </Grid>
+                <Grid item>
+                  <Button variant="contained" color="secondary">
+                    Login
+                  </Button>
+                </Grid>
+              </form>
+            </CardContent>
+         </Card>
         </Grid>
-        <Grid item>
-          <Button variant="contained" color="secondary">
-            Login
-          </Button>
-        </Grid>
-      </form>
-    </Grid>
+     
   );
 };
 
