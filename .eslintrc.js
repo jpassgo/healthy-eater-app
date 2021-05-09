1ff6fa5f441099c8645791050f687b2ddaa94a36
jpassgo <jeffpascoe9@gmail.com>
2021-05-09 08:55:47 -0500

    Fixing eslint config and setting eslint to only warn when there are unused vars.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,18 @@
 module.exports = {
-    extends: ['airbnb-typescript'],
-    parserOptions: {
-      project: './tsconfig.json',
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: ['airbnb-typescript'],
+      parserOptions: {
+        project: ['./tsconfig.json'],
+        "ecmaFeatures": {
+          "impliedStrict": false
+        },     
+      },
+      rules: {
+        "@typescript-eslint/no-unused-vars": "warn"
+      },
     },
+  ],
+  parser: '@typescript-eslint/parser',
   };
