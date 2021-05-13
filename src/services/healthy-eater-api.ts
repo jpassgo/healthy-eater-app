adb3d085e9c11f6869db40d32b47ea459dcfad21
jpassgo <jeffpascoe9@gmail.com>
2021-05-13 17:14:06 -0500

    Fixing authenticateUser function.

--- src/services/healthy-eater-api.ts
+++ src/services/healthy-eater-api.ts
@@ -2,16 +2,23 @@ import buildUrl from 'build-url';
 import urls from '../constants/urls';
 
 interface AuthorizationToken {
-  token: string,
+  authorization: string,
 }
 
 export default function authenticateUser(username: string, password: string): any {
+  const userName = username;
   return fetch(buildUrl(urls.healthyEaterApi, { path: '/authentication' }),
     {
       method: 'Post',
-      body: JSON.stringify({ username, password }),
+      body: JSON.stringify({ userName, password }),
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
     })
-    .then((response: Response) => response.json())
+    .then((response: Response) => {
+      response.json();
+    })
     .catch((error: Error) => {
       console.error(`Error authenticating user: ${error}`);
     });
