1fb4b40c2dac461699777b5719394880cb97cd0c
jpassgo <jeffpascoe9@gmail.com>
2021-05-25 18:13:31 -0500

    Cleaning up imports

--- src/components/CreateAccountPage.tsx
+++ src/components/CreateAccountPage.tsx
@@ -1,5 +1,4 @@
 import React, { ChangeEvent, useState } from 'react';
-import { Dispatch } from 'redux';
 import { connect } from 'react-redux';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
