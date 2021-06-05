65a855eb67dca4e896221b42553ae682b3d0a391
jpassgo <jeffpascoe9@gmail.com>
2021-06-04 22:10:01 -0500

    Updating accountCreationSuccessful action

--- src/creators/create-account.ts
+++ src/creators/create-account.ts
@@ -2,12 +2,8 @@ import { AnyAction } from 'redux';
 import actions from '../actions/actions';
 
 // eslint-disable-next-line import/prefer-default-export
-export const createAccount = (username: string, password: string,
-  firstName: string, lastName: string, emailAddress: string): AnyAction => ({
+export const accountCreationSuccessful = (username: string, token: string): AnyAction => ({
   type: actions.CREATE_ACCOUNT,
   username,
-  password,
-  firstName,
-  lastName,
-  emailAddress,
+  token,
 });
