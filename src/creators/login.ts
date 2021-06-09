60eeb2d577b1cbb4b16135ff299bff05b48852cc
jpassgo <jeffpascoe9@gmail.com>
2021-06-08 19:31:22 -0500

    There is no try, there is only do.

--- src/creators/login.ts
+++ src/creators/login.ts
@@ -1,12 +1,6 @@
 import { AnyAction } from 'redux';
 import actions from '../actions/actions';
 
-export const loginAttempt = (username: string, password: string): AnyAction => ({
-  type: actions.LOGIN_ATTEMPT,
-  username,
-  password,
-});
-
 export const loginSuccess = (token: string): AnyAction => ({
   type: actions.LOGIN_SUCCESS,
   token,
