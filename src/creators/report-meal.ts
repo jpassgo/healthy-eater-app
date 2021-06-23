6505f0fc40a1d5d334e7d4d94a5833d7924f8a70
jpassgo <jeffpascoe9@gmail.com>
2021-06-22 19:35:56 -0500

    Updating name of function.

--- src/creators/report-meal.ts
+++ src/creators/report-meal.ts
@@ -1,8 +1,8 @@
 import { AnyAction } from 'redux';
 import actions from '../actions/actions';
-import Meal from '../models/meal';
+import Meal from '../models/Meal';
 
 // eslint-disable-next-line import/prefer-default-export
-export const reportMeal = (): AnyAction => ({
+export const mealReported = (): AnyAction => ({
   type: actions.REPORT_MEAL,
 });
